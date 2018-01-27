3e8488cc5bde7a5d3b0654fa45367329e3c21dfe
SaeedS28 <awesomesaad.saeed2@gmail.com>
2018-01-27 16:52:07 -0500

    got the right file structure going, and tested it. Now to refactor the code in Start.java

--- studentCalc3/src/ctrl/Start.java
+++ studentCalc3/src/ctrl/Start.java
@@ -1,3 +1,4 @@
+package ctrl;
 
 import java.io.IOException;
 import java.io.Writer;
---
+++ studentCalc3/src/model/Loan.java
@@ -0,0 +1,30 @@
+package model;
+
+public class Loan {
+	
+	public double computePayment(double principal,double period, double interest, double fixedInterest, boolean grace, 
+			double graceInterest,double gracePeriod) throws Exception{
+		
+		double monthlyPayments;
+		double totalInterest = interest+ fixedInterest;
+		monthlyPayments = ((0.01 * totalInterest) / 12) * principal	/ (1 - Math.pow(1 + ((0.01 * totalInterest) / 12), (-1) * period)); 
+		
+		if(grace) {
+			monthlyPayments = monthlyPayments + (graceInterest/gracePeriod); 
+		}
+		return monthlyPayments;
+	}
+	
+	public static double computeGraceInterest(double principal, double gracePeriod, double interest,
+			double fixedInterest, boolean grace) throws Exception {
+		double graceInterest;
+		double totalInterest = interest+fixedInterest;
+		if(grace) {
+			graceInterest=principal*((totalInterest*0.01)/12)*gracePeriod;
+		} else {
+			graceInterest=0;
+		}
+		return graceInterest;
+	}
+
+}
