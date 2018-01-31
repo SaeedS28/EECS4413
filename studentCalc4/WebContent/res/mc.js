8d088948c32963675484b782622e0bd5e99cdc0c
SaeedS28 <awesomesaad.saeed2@gmail.com>
2018-01-31 13:30:37 -0500

    got the js to work. Done the lab.

--- studentCalc4/WebContent/res/mc.js
+++ studentCalc4/WebContent/res/mc.js
@@ -1,3 +1,28 @@
 /**
  * 
- */
+ */function validate(){
+	 var ok = true;
+	 
+	 var p = document.getElementById("principal").value;
+	 if (isNaN(p) || p < 0){
+		 alert("Principal invalid! Must be non-negative numnber");
+		ok = false;
+	 }
+	 
+	 if (!ok) return false;
+	 
+	 p = document.getElementById("interest").value;
+	 if (isNaN(p) || p < 0 || p >= 100){
+		 alert("Interest invalid. Must be in [0,100].");
+		 ok = false;
+	 }
+	 
+	 if (!ok) return false;
+	 
+	 p = document.getElementById("period").value;
+	 if (isNaN(p) || p < 0){
+		 alert("Period invalid. Must be greater than 0.");
+		 ok = false;
+}
+return ok;
+}
