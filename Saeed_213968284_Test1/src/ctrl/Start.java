8edb8cd9ac2b07ccf62bd01d41af0a1ea730df0f
SaeedS28 <awesomesaad.saeed2@gmail.com>
2018-02-07 00:30:23 -0500

    redirection screws up

--- Saeed_213968284_Test1/src/ctrl/Start.java
+++ Saeed_213968284_Test1/src/ctrl/Start.java
@@ -30,6 +30,9 @@ public class Start extends HttpServlet {
 	private static final String MONTHLY_PAYMENT = "monthlyPayment";
 	private static final String ERRROR_MESSAGE = "errorMessage";
 
+	String path;
+	String last;
+	boolean isNormal=false;
 	private String errorMessage = "";
 	private boolean errorOccured = false;
 	private double graceInterest;
@@ -79,92 +82,105 @@ public class Start extends HttpServlet {
 			throws ServletException, IOException {
 		Writer ro = response.getWriter();
 		
-		String path = request.getRequestURI();
-		String last = path.substring(path.lastIndexOf('/') + 1);
+		path = request.getRequestURI();
+		last = path.substring(path.lastIndexOf('/') + 1);
 		System.out.println(last);
 		
 		if(last.toString().contains("Fixed")) {
+			isNormal=true;
 			ro.append("This url contains Fixed \n");
 			request.setAttribute(FIXED, "blah");
 			System.out.println("Contains fixed");
 			System.out.println("Value contained within the FIXED variable is " + getServletContext().getAttribute(FIXED));
 		}
 		else {
+			isNormal=false;
 			System.out.println("doesn't contain fixed");
 			System.out.println("Value contained within the FIXED variable is " + getServletContext().getAttribute(FIXED));
 		}
-//		else{
-//			paymentCalc(request, response);		
-//			DecimalFormat df = new DecimalFormat("#.####");
-//			df.setMaximumFractionDigits(2);
-//
-//			getServletContext().setAttribute(INTEREST, interest);
-//			getServletContext().setAttribute(PERIOD, period);
-//			getServletContext().setAttribute(PRINCIPAL, principal);
-//			getServletContext().setAttribute(MONTHLY_PAYMENT, df.format(totalPrincipal));
-//			getServletContext().setAttribute(GRACE_INTEREST, df.format(graceInterest));
-//			
-//			String dispatchURL = startPage;
-//			String submit = request.getParameter("Submit");
-//			String restart = request.getParameter("restart");
-//
-//			if (restart != null && restart.equals("Recompute")) {
-//				errorMessage = "";
-//				displayError();
-//			} else if (submit != null && submit.equals("submit")) {
-//				if (!errorOccured) {
-//					dispatchURL = resultPage;
-//				}
-//			}
-//			//request.getRequestDispatcher(startPage).forward(request, response);
-//		}
-		request.getRequestDispatcher(startPage).forward(request, response);
+
+		 paymentCalc(request, response);
+		 DecimalFormat df = new DecimalFormat("#.####");
+		 df.setMaximumFractionDigits(2);
+		
+		 getServletContext().setAttribute(INTEREST, interest);
+		 getServletContext().setAttribute(PERIOD, period);
+		 getServletContext().setAttribute(PRINCIPAL, principal);
+		 getServletContext().setAttribute(MONTHLY_PAYMENT, df.format(totalPrincipal));
+		 getServletContext().setAttribute(GRACE_INTEREST, df.format(graceInterest));
+		
+		 String dispatchURL = startPage;
+		 String submit = request.getParameter("Submit");
+		 String restart = request.getParameter("restart");
+		
+		 if (restart != null && restart.equals("Recompute")) {
+			 errorMessage = "";
+			 displayError();
+		 } else if (submit != null && submit.equals("submit")) {
+			 if (!errorOccured) {
+				 dispatchURL = resultPage;
+			 }
+		 }
+		request.getRequestDispatcher(dispatchURL).forward(request, response);
 	}
 
-//	private void paymentCalc(HttpServletRequest request, HttpServletResponse response)
-//			throws ServletException, IOException {
-//
-//		String principalInput = request.getParameter("principal");
-//		String periodInput = request.getParameter("period");
-//		String interestInput = request.getParameter("interest");
-//
-//		errorOccured = false;
-//
-//		double fixedInterest1 = Double.parseDouble(this.getServletContext().getInitParameter("fixedInterest"));
-//		double gracePeriod = Double.parseDouble(this.getServletContext().getInitParameter("gracePeriod"));
-//
-//		try {
-//			principal = Double.parseDouble(principalInput);
-//			period = Double.parseDouble(periodInput);
-//			interest = Double.parseDouble(interestInput);
-//			graceInterest = loan.computeGraceInterest(principal, gracePeriod, interest, fixedInterest1,
-//					graceCheckedOff(request), period);
-//			totalPrincipal = loan.computePayment(principal, period, interest, fixedInterest1, graceCheckedOff(request),
-//					graceInterest, gracePeriod);
-//		} catch (NumberFormatException e) {
-//			errorOccured = true;
-//			errorMessage = "";
-//			errorMessage = "You entered something that is not a number. Try again";
-//			displayError();
-//		} catch (IllegalArgumentException e) {
-//			errorOccured = true;
-//			//
-//			errorMessage = "";
-//			if (principal < 0) {
-//				errorMessage = errorMessage + "Principal value cannot be negative. \n";
-//			}
-//			if (period < 0) {
-//				errorMessage = errorMessage + "Period value cannot be negative. \n";
-//			}
-//			if (interest < 0) {
-//				errorMessage = errorMessage + "Interest value cannot be negative.\n ";
-//			}
-//			displayError();
-//		} catch (Exception e) {
-//			errorOccured = true;
-//		}
-//	}
+	private void paymentCalc(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
+		String principalInput = request.getParameter("principal");
+		String periodInput = request.getParameter("period");
+		String interestInput = request.getParameter("interest");
+
+		errorOccured = false;
+		try {
+			if (isNormal == true) {
+				String lastChar = last.substring(last.length() - 1);
+				System.out.println("Value of last char is " + lastChar);
+				if (lastChar.matches("[A-F]")) {
+					period = 10;
+				} else if (lastChar.matches("[G-K]")) {
+					period = 15;
+				} else if (lastChar.matches("[L-O]")) {
+					period = 20;
+				} else if (lastChar.matches("[P-Z]")) {
+					period = 30;
+				}
+			}else {
+				period = Double.parseDouble(periodInput);
+			}
+			System.out.println("Value of period is " + period);
+			double fixedInterest1 = Double.parseDouble(this.getServletContext().getInitParameter("fixedInterest"));
+			double gracePeriod = Double.parseDouble(this.getServletContext().getInitParameter("gracePeriod"));
+
+			principal = Double.parseDouble(principalInput);
+			
+			interest = Double.parseDouble(interestInput);
+			graceInterest = loan.computeGraceInterest(principal, gracePeriod, interest, fixedInterest1,
+					graceCheckedOff(request), period);
+			totalPrincipal = loan.computePayment(principal, period, interest, fixedInterest1, graceCheckedOff(request),
+					graceInterest, gracePeriod);
+		} catch (NumberFormatException e) {
+			errorOccured = true;
+			errorMessage = "";
+			errorMessage = "You entered something that is not a number. Try again";
+			displayError();
+		} catch (IllegalArgumentException e) {
+			errorOccured = true;
+			errorMessage = "";
+			if (principal < 0) {
+				errorMessage = errorMessage + "Principal value cannot be negative. \n";
+			}
+			if (period < 0) {
+				errorMessage = errorMessage + "Period value cannot be negative. \n";
+			}
+			if (interest < 0) {
+				errorMessage = errorMessage + "Interest value cannot be negative.\n ";
+			}
+			displayError();
+		} catch (Exception e) {
+			errorOccured = true;
+		}
+	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
