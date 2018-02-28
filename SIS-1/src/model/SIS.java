ead676afa3f5fb53c04b3729d3fa73bff0b30009
Saad Saeed <saeeds28@gsp18.eecs.yorku.ca>
2018-02-28 11:26:42 -0500

    Deleted the DB on git because it was fucking up

--- SIS-1/src/model/SIS.java
+++ SIS-1/src/model/SIS.java
@@ -16,12 +16,23 @@ public class SIS {
 	}
 
 	public Map<String, StudentBean> retriveStudent(String namePrefix, String credit_taken) throws Exception {
-		return null;
-
+		int creds=0;
+		try{
+			creds=Integer.parseInt(credit_taken);
+		} catch(Exception e){
+			System.out.println(" you fucked up fam");
+		}
+		return sd.retrieve(namePrefix, creds);
 	}
 
-	public Map<String, EnrollmentBean> retriveEnrollment() throws Exception {
-		return null;
+	public Map<String, EnrollmentBean> retriveEnrollment(String namePrefix, String credit_taken) throws Exception {
+		int creds=0;
+		try{
+			creds=Integer.parseInt(credit_taken);
+		} catch(Exception e){
+			System.out.println(" you fucked up fam");
+		}
+		return ed.retrieve(namePrefix, creds);
 
 	}
 
