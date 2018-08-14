c910e7aa106d907a4daa74380f7c880fdb656d0e
SaeedS28 <awesomesaad.saeed2@gmail.com>
2018-08-14 00:39:47 -0400

    this works without screwing up lab 6

--- SIS-1/src/bean/ListWrapper.java
+++ SIS-1/src/bean/ListWrapper.java
@@ -2,6 +2,7 @@ package bean;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlAttribute;
@@ -10,46 +11,22 @@ import javax.xml.bind.annotation.XmlElement;
 @XmlRootElement(name="sisReport")
 public class ListWrapper {
 	
-	@XmlAttribute(name="namePrefix")
+	@XmlAttribute
 	private String namePrefix;
 	@XmlAttribute(name = "creditTaken")
-	private int credit_taken;
+	private String credit_taken;
 	@XmlElement(name="studentList")
 	private List<StudentBean> list;
 	
 	//Default ctor
 	public ListWrapper() {
-		
+		this("","",new ArrayList<StudentBean>());
 	}
 	
 	//overloaded ctor
-	public ListWrapper(String namePrefix, int credit_taken, List<StudentBean> list) {
+	public ListWrapper(String namePrefix, String credit_taken, List<StudentBean> list) {
 		this.namePrefix = namePrefix;
 		this.credit_taken = credit_taken;
-		this.list = list;
-	}
-	
-	public String getNamePrefix() {
-		return namePrefix;
-	}
-	
-	public void setNamePrefix(String namePrefix) {
-		this.namePrefix = namePrefix;
-	}
-	
-	public int getCredit_taken() {
-		return credit_taken;
-	}
-	
-	public void setCredit_taken(int credit_taken) {
-		this.credit_taken = credit_taken;
-	}
-	
-	public List<StudentBean> getList() {
-		return list;
-	}
-	
-	public void setList(List<StudentBean> list) {
 		this.list = list;
 	}
 
--- SIS-1/src/model/SIS.java
+++ SIS-1/src/model/SIS.java
@@ -1,6 +1,14 @@
 package model;
 
+import java.io.FileWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.Map;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.transform.stream.StreamResult;
+
+import com.sun.xml.internal.ws.util.Pool.Marshaller;
 
 import DAO.*;
 import bean.*;
@@ -37,7 +45,32 @@ public class SIS {
 		return null;
 	}
 
-	public void export(String namePrefix, String minGPA, String filename) throws Exception{
+	public void export(String namePrefix, String credit_taken, String filename) throws Exception{
+		int credit=0;
 		
+		try {
+			credit=Integer.parseInt(credit_taken);
+			//StudentDAO students = new StudentDAO();
+			Map<String,StudentBean> sb=sd.retrieve(namePrefix, credit);
+			ListWrapper lw=new ListWrapper(namePrefix, credit_taken, new ArrayList<StudentBean>(sb.values()));
+			
+			JAXBContext jc = JAXBContext.newInstance(lw.getClass());
+			javax.xml.bind.Marshaller marshaller=jc.createMarshaller();
+			marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+			marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
+			
+			StringWriter sw = new StringWriter();
+			
+			sw.write("\n");
+			marshaller.marshal(lw, new StreamResult(sw));
+			
+			System.out.println(sw.toString());
+			FileWriter fw = new FileWriter(filename);
+			fw.write(sw.toString());
+			fw.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+			
 	}
 }
